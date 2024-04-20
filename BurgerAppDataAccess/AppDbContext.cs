7a78790adaec20c39d2f0448b19a4f780bef7a6d
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-04-21 01:34:56 +0300

    Add project files.

---
+++ BurgerAppDataAccess/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurgerAppDataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=HASAN_YURDAKUL;Database=BurgerApp;User ID=sa;Password=1q2w3e4r;TrustServerCertificate=True");
+        }
+    }
+}
